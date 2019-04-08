a8f8fd07c8cd9a649c46ca9d20cb6927cc2b667f
Will Holmes <willholmes97@icloud.com>
2019-04-08 22:35:08 +0100

    Added a base implementation of the landing ui

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import ModuleFinder from '../../components/moduleFinder';
+import Header from '../header';
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/search">Search</Link>
-    </header>
-
-    <main>
+  <Header />
+    <main style={{ padding: 25 }}>
       <Route exact path="/" component={ModuleFinder} />
       <Route exact path="/search" component={ModuleFinder} />
     </main>
--- src/components/moduleFinder/index.jsx
+++ src/components/moduleFinder/index.jsx
@@ -1,54 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { requestNpmModule } from '../../actions';
+import { requestNpmModule } from '../../actions'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 
 class ModuleFinder extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  
-            moduleSearchName: ''
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      moduleSearchName: ''
     }
+  }
 
-    render() { 
-        const { onRequestModule, module, isFetching, error } = this.props;
-        const { moduleSearchName } = this.state;
-        return ( 
-        <div>
-            <input type="text" placeholder="Enter module name" onChange={evt => this.setState({ moduleSearchName: evt.target.value })} />
-            <button type="button" onClick={() => onRequestModule(moduleSearchName)} >Search </button>
+  render() {
+    const { onRequestModule, module, isFetching, error } = this.props
+    const { moduleSearchName } = this.state
+    return (
+      <Fragment>
+        <Grid
+          container
+          spacing={24}
+          justify="center"
+          direction="row"
+          alignItems="center">
+          <Grid item xs={12} style={{ textAlign: 'center' }}>
+            <h1>NpmGui</h1>
+            <h3>Find your npm package now...</h3>
+          </Grid>
+          <Grid item xs={12} sm={6} style={{ textAlign: 'center' }}>
+            <Paper elevation={1} style={{ padding: 20 }}>
+              <TextField
+                id="standard-full-width"
+                placeholder="Enter npm module name..."
+                fullWidth
+                style={{ paddingLeft: 10, paddingRight: 10 }}
+                margin="normal"
+                onChange={evt =>
+                  this.setState({ moduleSearchName: evt.target.value })
+                }
+                InputLabelProps={{
+                  shrink: true
+                }}
+              />
+              <Button
+                size="large"
+                color="primary"
+                variant="contained"
+                style={{ marginTop: 15, textAlign: 'center', width: 250 }}
+                onClick={() => onRequestModule(moduleSearchName)}>
+                Search
+              </Button>
+            </Paper>
 
-            {
-                isFetching &&
-                <p>fetching...</p>
-            }
-            {
-                module !== {} &&
-                <React.Fragment>
-                    <h3>{module.name}</h3>
-                    <p>{module.description}</p>
-                </React.Fragment>
-            }
-            {
-                error.length > 0 &&
-                <h3>Error: {error}</h3>
-            }
-        </div>
-         );
-    }
+            {isFetching && <p>fetching...</p>}
+            {module !== {} && (
+              <React.Fragment>
+                <h3>{module.name}</h3>
+                <p>{module.description}</p>
+              </React.Fragment>
+            )}
+            {error.length > 0 && <h3>Error: {error}</h3>}
+          </Grid>
+        </Grid>
+      </Fragment>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-    module: state.module,
-    isFetching: state.loading,
-    error: state.error,
+  module: state.module,
+  isFetching: state.loading,
+  error: state.error
 })
 
 const mapDispatchToProps = dispatch => ({
-    onRequestModule: moduleName => dispatch(requestNpmModule(moduleName))
+  onRequestModule: moduleName => dispatch(requestNpmModule(moduleName))
 })
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(ModuleFinder)
